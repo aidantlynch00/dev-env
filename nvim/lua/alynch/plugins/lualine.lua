6ed0b0476d9ee7cfe3b81137dc2fef232194de27
Aidan Lynch <aidantlynch00@gmail.com>
2024-11-23 16:01:38 -0500

    Reworked neovim statusline
    Changed the lualine configuration to remove unnecessary information and
    to include the current symbol tree location as reported by aerial.

--- nvim/lua/alynch/plugins/lualine.lua
+++ nvim/lua/alynch/plugins/lualine.lua
@@ -1,5 +1,13 @@
 require("lualine").setup {
     options = {
         theme = "tokyonight"
-    }
+    },
+    sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { "filename", "aerial" },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+    },
 }
