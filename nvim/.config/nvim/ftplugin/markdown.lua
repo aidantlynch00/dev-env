0ab6e7d3c4deb631a70e65be2dff60fedd9d82d6
Aidan Lynch <aidantlynch00@gmail.com>
2026-01-28 16:39:25 -0500

    Added word wrap and vertical motions for textual files

---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+-- configure word wrapping
+vim.o.wrap = true
+vim.o.linebreak = true
+
+-- use j and k motions for wrapped lines
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
