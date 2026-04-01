13db46f67c797d0e62fd52ce4003db4f309ff25c
Aidan Lynch <aidantlynch00@gmail.com>
2026-04-01 14:48:43 -0400

    Reworked nvim-treesitter config for main branch installation

--- nvim/.config/nvim/lua/alynch/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/alynch/plugins/treesitter.lua
@@ -1,13 +1,37 @@
-require("nvim-treesitter").setup {
-    ensure_installed = {
-        "c", "cpp", "rust",      -- compiled languages
-        "lua", "bash", "python", -- scripting languages
-        "html", "css",           -- web languages
-        "make",                  -- tooling
-        "xml", "json", "csv",    -- data formats
-        "markdown"               -- text formats
-    },
-    highlight = {
-        enable = true
-    }
+local languages = {
+    "c",
+    "cpp",
+    "rust",
+    "lua",
+    "bash",
+    "python",
+    "javascript",
+    "html",
+    "css",
+    "make",
+    "xml",
+    "json",
+    "csv",
+    "markdown",
 }
+
+require('nvim-treesitter').install(languages)
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(args)
+        local buf, filetype = args.buf, args.match
+
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+            return
+        end
+
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then
+            return
+        end
+
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+    end,
+})
