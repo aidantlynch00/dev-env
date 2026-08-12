25938efb270bf21f7745843977053595805e1057
Aidan Lynch <aidantlynch00@gmail.com>
2026-08-12 13:17:01 -0700

    Tweaks to blink.cmp config
    Turned preselect off and disabled bracket insertion on accept. Added
    additional binds to scroll selection menu. Improved escape bind to
    cancel completion (close the menu) and leave insert mode which mimics
    coq.nvim behavior.

--- nvim/.config/nvim/lua/alynch/plugins/blink.lua
+++ nvim/.config/nvim/lua/alynch/plugins/blink.lua
@@ -1,20 +1,39 @@
 local blink = require("blink.cmp")
 
+local cancel_cmp_and_enter_normal = function(cmp)
+    cmp.cancel()
+    vim.schedule(vim.cmd.stopinsert)
+end
+
 blink.setup({
     keymap = {
         preset = "none",
         ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<Esc>"] = { "cancel", "fallback" },
+        ["<Esc>"] = { cancel_cmp_and_enter_normal },
         ["<BS>"] = { "fallback" },
         ["<CR>"] = { "accept", "fallback" },
         ["<Tab>"] = { "select_next", "fallback" },
         ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
     },
     appearance = {
         nerd_font_variant = "mono",
     },
     completion = {
-        documentation = { auto_show = false },
+        list = {
+            selection = {
+                preselect = false,
+            },
+        },
+        documentation = {
+            auto_show = false,
+        },
+        accept = {
+            auto_brackets = {
+                enabled = false,
+            },
+        },
     },
     sources = {
         default = { "lsp", "path", "snippets", "buffer" },
